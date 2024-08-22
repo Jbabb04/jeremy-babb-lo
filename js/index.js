bb92883081c8fd5e07f7f4708130fdc364a0461f
Jeremy <jeremybabb@gmail.com>
2024-08-22 19:40:21 -0400

    js, added created footer and skills but not showing up in preview

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const footer = document.createElement ('footer'); 
+const body = document.querySelector('body');
+body.appendChild(footer); 
+
+const today = new Date();
+const year = today.getFullYear();
+const copyright = document.createElement('p');
+copyright.innerHTML = `<span>Jeremy</span><span>&#169</span><span>${year}</span>`
+footer.appendChild(copyright);
+
+const skillsList = ['Javascript', 'HTML', 'CSS', 'GitHub'];
+const skillsSection = document.getElementById('skills'); 
+const skillsUL = skillsSection.querySelector('ul');
+
+for (let skill of skillsList) {
+    let skillLI = document.createElement('li');
+    skillLI.innerHTML = skill;
+    skillsUL.appendChild(skillLI);
+}
